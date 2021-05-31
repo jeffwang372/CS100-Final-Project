c8ee1ccc8d2eeab7d1860d206c38f1ca9c3dcd51
jeffwang372 <jeffwang372@gmail.com>
2021-05-30 17:02:23 -0700

    Completed input validation for adding and removing tasks

--- taskscomposite.cpp
+++ taskscomposite.cpp
@@ -2,10 +2,9 @@
 #include <iostream>
 
 TasksComposite::~TasksComposite() {
-	//delete this;
 }
 
-TasksComposite::TasksComposite(string time, string date, string duration, string location, string subject, string body, int ID) {
+TasksComposite::TasksComposite(string time, string date, double duration, string location, string subject, string body, int ID) {
 	this->time = time;
 	this->date = date;
 	this->duration = duration;
@@ -13,14 +12,13 @@ TasksComposite::TasksComposite(string time, string date, string duration, string
 	this->subject = subject;
 	this->body = body;
 	this->ID = ID;
-	cout << "TEST: TaskComposite Object created" << endl;
 }
 
 string TasksComposite::getDate() {
 	return this->date;
 }//date getter
 
-string TasksComposite::getDuration() {
+double TasksComposite::getDuration() {
         return this->duration;
 }//duration getter
 
